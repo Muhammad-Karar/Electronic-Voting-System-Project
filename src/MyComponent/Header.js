0b414e6c2bea9e2a7d644a4bcbeaf6070b9c80aa
Muhammad Karar <muhammadkarar145@gmail.com>
2023-07-07 15:41:46 -0700

    header and home updated

--- src/MyComponent/Header.js
+++ src/MyComponent/Header.js
@@ -1,10 +1,28 @@
-import React from 'react'
+
+import React from 'react';
+import pic1 from '../pic1jpeg.jpeg'
 
 export default function Header() {
   return (
     <div>
-      <h1>hello</h1>
-      <h2>world</h2>
+      <section Class="main-navigation">
+        <div Class="nav-logo">
+          <img src={pic1} className='logo' alt="logo" />
+            <h3><span>E</span>voting</h3>
+        </div>
+        <div Class="nav-list">
+            <a href="#home">Home</a>
+            <a href="#profile">About</a>
+            <a href="#services-section">Cnadidates</a>
+            <a href="#recent-section">Result</a>
+            <a href="#contact-section">Contact</a>
+        </div>
+        <div Class="registration-sec">
+            <a href="">Sign up</a>
+            <a href="">Login</a>
+        </div>
+    </section>
+
     </div>
   )
-}
+  }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './MyComponent/Header';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    
+    <App />
   </React.StrictMode>
 );
 
