d85c369fa3286ac90758d9a2ab110063978f2e72
Muhammad Karar <muhammadkarar145@gmail.com>
2023-07-29 16:27:05 -0700

    updated

--- src/MyComponent/About.js
+++ src/MyComponent/About.js
@@ -7,12 +7,48 @@ export default function About() {
       <div className="abt-main-div">
         <div className="abt-content">
           <div className="abt-heading">
-          <h1>About</h1>
+            <h1>About</h1>
           </div>
           <div className="abt-writing">
-            <h1>content</h1>
+            <p>Our Mission
+
+              Our mission is to empower citizens to exercise their voting rights conveniently, while ensuring the utmost security and accuracy of election results. By leveraging the latest advancements in technology, we aim to bridge the gap between voters and their participation in the democratic process.
+
+              Key Features
+
+              Security: The security of our Electronic Voting System is our highest priority. We have implemented robust encryption protocols, multi-factor authentication, and continuous monitoring to safeguard against potential threats. Additionally, our system adheres to international cybersecurity standards to provide maximum protection for sensitive voter data.
+
+              Accessibility: We believe in an inclusive democracy, and our platform is designed to be accessible to all eligible voters, regardless of their physical abilities or location. Our user-friendly interface ensures that even those with limited technical knowledge can easily cast their ballots.
+
+              Transparency: Our Electronic Voting System is built on a transparent framework, allowing voters and election administrators to track and verify the entire voting process. This transparency fosters trust in the electoral system and minimizes the likelihood of fraudulent activities.
+
+              Auditability: The system offers a comprehensive audit trail, enabling election authorities and stakeholders to conduct thorough post-election audits for complete confidence in the results.
+
+              Scalability: Whether it's a local election or a national referendum, our platform is designed to handle a large number of voters concurrently, ensuring a seamless voting experience even during peak times.
+
+              Real-time Results: Through our system, election results are tabulated and available in real-time, providing instant insights into the voting outcome.
+
+              How It Works
+
+              Voter Registration: Eligible voters can easily register on our platform, providing necessary identification and authentication information.
+
+              Secure Authentication: Once registered, voters will receive unique login credentials through secure channels, ensuring the identity of each voter.
+
+              Casting Votes: On the election day or during the specified voting period, voters can log in to the system and cast their votes electronically. The platform ensures that each vote is anonymous and cannot be traced back to the individual voter.
+
+              Vote Tabulation: The votes are securely stored and tabulated automatically, guaranteeing accuracy and efficiency in the result calculation process.
+
+              Results Declaration: As soon as the voting period ends, the system generates the election results in real-time, which can be accessed by authorized personnel and the public.
+
+              Trust in Democracy, Trust in Us
+
+              At [Company Name], we are committed to upholding the fundamental principles of democracy and providing a reliable, secure, and accessible electronic voting solution. Our team of experts works tirelessly to stay ahead of emerging threats and continually improves the platform to deliver the best voting experience possible.
+
+              Join us in shaping the future of democracy through technology. Together, let's build a more engaged and participative society!
+
+              For inquiries or more information, please contact us at Contact Email/Phone Number</p>
           </div>
-          
+
         </div>
       </div>
 
--- src/MyComponent/Candidates.js
+++ src/MyComponent/Candidates.js
@@ -4,11 +4,12 @@ import cpic2 from '../cpic2.jpg';
 import cpic3 from '../cpic3.jpg';
 import cpic4 from '../cpic4.jpg';
 import cpic5 from '../cpic5.jpeg';
-import cpic6 from '../cpic6.jpg'
+import cpic6 from '../cpic6.jpg';
+import ptiflag from '../ptiflag.png';
 export default function Candidates() {
     return (
-        <div style={{ backgroundColor: "rgb(74, 75, 75)",display: "flex", justifyContent: "center" }}>
-            <div  style={{marginLeft: "200px",paddingTop: "30px",color: "white" }} Class="container">
+        <div style={{ backgroundColor: "rgb(74, 75, 75)",display: "flex", justifyContent: "center", height: "90vh" }}>
+            <div  style={{marginLeft: "200px",paddingTop: "50px",color: "white" }} Class="container">
 
 
                     <div class="row">
@@ -16,42 +17,42 @@ export default function Candidates() {
                             {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns={cpic1} role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg> */}
                             <img style={{width: "140px",height: "140px" }} class="bd-placeholder-img rounded-circle " src={cpic1} alt="" />
                             <h2 class="fw-normal">Imran Khan</h2>
-                            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                            <p>Chaiman (Pakistan Tehrek-e-Insaf).</p>
                             <p><a class="btn btn-outline-light" href="#">Cast vote »</a></p>
                         </div>
                         <div class="col-lg-4">
                             {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg> */}
                             <img style={{width: "140px",height: "140px" }} class="bd-placeholder-img rounded-circle " src={cpic2} alt="" />
                             <h2 class="fw-normal">shahbaz sharif</h2>
-                            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                            <p>Chaiman (Pakistan Muslim League-N).</p>
                             <p><a class="btn btn-outline-light" href="#">Cast vote »</a></p>
                         </div>
                         <div class="col-lg-4">
                             {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg> */}
                             <img style={{width: "140px",height: "140px" }} class="bd-placeholder-img rounded-circle " src={cpic3} alt="" />
                             <h2 class="fw-normal">Bilawal Bhutto</h2>
-                            <p>And lastly this, the third column of representative placeholder content.</p>
+                            <p>Chaiman (Pakistan Peoples Party).</p>
                             <p><a class="btn btn-outline-light" href="#">Cast vote »</a></p>
                         </div>
                         <div class="col-lg-4">
                             {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg> */}
                             <img style={{width: "140px",height: "140px" }} class="bd-placeholder-img rounded-circle " src={cpic4} alt="" />
                             <h2 class="fw-normal">Aimal wali khan</h2>
-                            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                            <p>Chaiman (Awami National Party).</p>
                             <p><a class="btn btn-outline-light" href="#">Cast vote »</a></p>
                         </div>
                         <div class="col-lg-4">
                             {/* <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg> */}
                             <img style={{width: "140px",height: "140px" }} class="bd-placeholder-img rounded-circle " src={cpic5} alt="" />
                             <h2 class="fw-normal">Siraj ul Haq</h2>
-                            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                            <p>Chaiman (Jamiat Ulema-e-Islam).</p>
                             <p><a class="btn btn-outline-light" href="#">Cast vote »</a></p>
                         </div>
                         <div class="col-lg-4">
                             {/* <svg class="bd-placeh/older-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg> */}
                             <img style={{width: "140px",height: "140px" }} class="bd-placeholder-img rounded-circle " src={cpic6} alt="" />
                             <h2 class="fw-normal">shujaat hussain</h2>
-                            <p>And lastly this, the third column of representative placeholder content.</p>
+                            <p>Chaiman (Pakistan Muslim League-Q).</p>
                             <p><a class="btn btn-outline-light" href="#">Cast vote »</a></p>
                         </div>
                     </div>
