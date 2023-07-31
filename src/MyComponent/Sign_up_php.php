8bced82289cc91a7a92e97633a5effb14f97bde7
Muhammad Karar <muhammadkarar145@gmail.com>
2023-07-30 20:08:49 -0700

    changed

--- src/MyComponent/Sign_up_php.php
+++ src/MyComponent/Sign_up_php.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "your_database_server";
-$username = "your_database_username";
-$password = "your_database_password";
-$dbname = "your_database_name";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "evoting";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,10 +17,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $cnic = $_POST['cnic'];
     $dob = $_POST['dob'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $pass = $_POST['pass'];
 
     // Prepare and bind the insert statement
-    $stmt = $conn->prepare("INSERT INTO users (name, cnic, dob, email, password) VALUES (?, ?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO users (name, cnic, dob, email, password) VALUES ('$name', '$cnic', '$dob', '$email', '$pass'");
     $stmt->bind_param("sssss", $name, $cnic, $dob, $email, $password);
 
     if ($stmt->execute()) {
